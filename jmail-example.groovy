9e7383929326bc1be8fecc1c5bb0b238505870d7
Chance Gray <chance@mail.usf.edu>
2013-06-26 14:08:40 -0400

    file transfer

--- jmail-example.groovy
+++ jmail-example.groovy
@@ -1,5 +1,7 @@
 import javax.mail.*
 import javax.mail.internet.*
+import javax.activation.*;
+
 
 port = 25
 // fixture = new EmailFixture(port)
@@ -18,9 +20,36 @@ msg.sentDate = new Date()
 msg.subject = 'Successful build'
 msg.setRecipient(Message.RecipientType.TO, devteam)
 //msg.setRecipient(Message.RecipientType.CC, partners)
-msg.setHeader('Organization', 'mycompany.org')
-msg.setContent('Successful build for ' + new Date(),
-               'text/plain')
+msg.setHeader('Organization', 'USF-IT')
+//commented for attachement msg.setContent('Successful build for ' + new Date(),
+//commented for attachement             'text/plain')
+
+
+messageBodyPart = new MimeBodyPart()
+messageBodyPart.setText("""
+	Good morning, (Sponsor):
+Thank you for being a Sponsor in our VIP System. As agreed, we will contact you to confirm the individuals you sponsored are still affiliated with USF and they still require the access to services that was originally requested by you on their behalf.
+The following individuals have accounts that are set to expire, resulting in loss of services, in the next _____ days if no further action is taken by you as their Sponsor:
+Last Name, First Name Last Name, First Name Last Name, First Name
+Please provide a status for each person listed, including their current relationship with USF, so we can take the appropriate action on their account.
+We appreciate your time and assistance in ensuring only authorized persons have access to IT services and resources, thus protecting the information contained therein.
+If you have any questions, please contact IT-Security@usf.edu for more information.
+Thank you.
+Kay Svendgard
+VIP System Owner
+USF-IT- Office of Information Security
+	""")
+multipart = new MimeMultipart()
+multipart.addBodyPart(messageBodyPart)
+
+//add the attachment
+		messageBodyPart = new MimeBodyPart();
+         String filename = "file.txt";
+         DataSource source = new FileDataSource(filename);
+         messageBodyPart.setDataHandler(new DataHandler(source));
+         messageBodyPart.setFileName(filename);
+         multipart.addBodyPart(messageBodyPart);
+ msg.setContent(multipart )
 
 // Send the message
 Transport.send(msg)
