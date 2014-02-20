58f1386cf55e09ec93239e2176448779b3d08cbb
Chance Gray <chance@mail.usf.edu>
2014-02-20 16:26:27 -0500

    Just need to add an if statement and iterator should recipientHdr be specified;  then maybe some example templates and csv files;  also thinking about ability to handle json as well as csv

--- src/main/groovy/edu/usf/cims/emailer/emailer.groovy
+++ src/main/groovy/edu/usf/cims/emailer/emailer.groovy
@@ -17,7 +17,7 @@ import static com.xlson.groovycsv.CsvParser.parseCsv
 class Emailer {
 
 	def myTemplate
-	static def version = "0.0.1"
+	static def version = "20140220-Alpha"
 	def conf = new ConfigObject()
 
 	public static void main(String[] args) {
@@ -25,16 +25,27 @@ class Emailer {
 	try {			
 		def opt = getCommandLineOptions(args)
 		def conf = getConfigSettings(opt)
-		def props = conf.toProperties()
-		def CSVContents = retrieveCSVContents(opt)
-		//TODO: maybe the group by can be passed in
-		//def groups = [ groups : CSVContents.groupBy {"${it.gid}-${it.created_vipid}"}.values() ]
-		//BTW this works
+
+		//get data
+		def CSVContents = retrieveCSVContents(conf)
+		
+		//create template
 		def templateData = [ templateData : CSVContents ]
 
+		//process the template
 		def myTemplate = runTemplate(conf,templateData)
-		println myTemplate
-		//sendEmail(props,myTemplate)
+
+		// final action
+		if (opt.debug) {
+
+			println myTemplate
+			println ""
+			println conf
+			println ""
+		
+			} else {
+				sendEmail(conf, myTemplate)
+			}
 
 		}catch(Exception e) {
 			exitOnError e.message
@@ -49,16 +60,19 @@ class Emailer {
 						header:"\nAvailable options (use -h for help):\n",
 						width:100)
 
-		cli.with {
-			h longOpt:'help', 'usage information', required: false
-			v longOpt:'version', 'version information', required: false  
-			i longOpt:'inputFile', args:1, argName:'inputFile', 'csv file with email address and template values'
-			f longOpt:'fromAddr', args:1, argName:'fromAddr', 'address of the sender'
-			t longOpt:'template', args:1, argName:'template', 'template of the message'
-			e longOpt:'emailHdr', args:1, argName:'emailHdr', 'Header in csv file to identify column with recipient email addresses'
 		
-			_ longOpt:'defaults', args:1, argName:'configFileName', 'groovy config file', required: false
-		}
+		cli.h longOpt:'help', 'usage information', required: false
+		cli.v longOpt:'version', 'version information', required: false 
+		cli.d longOpt:'debug', 'Turn on debugging', required: false 
+		cli.i longOpt:'inputFile', args:1, argName:'inputFile', 'csv file with email address and template values'
+		cli.f longOpt:'fromAddr', args:1, argName:'fromAddr', 'address of the sender'
+		cli.t longOpt:'template', args:1, argName:'template', 'template of the message'
+		cli.r longOpt:'recipient', args:1, argName:'recipient', 'single recipient'
+		//TODO: add the possibility of a per message custom subject heading?
+		cli.s longOpt:'subject', args:1, argName:'subject', 'subject heading of the message'
+		cli.e longOpt:'recipientHdr', args:1, argName:'recipientHdr', 'Header in csv file to identify column with recipient email addresses'
+		cli._ longOpt:'defaults', args:1, argName:'configFileName', 'groovy config file', required: false
+		
 
 		def options = cli.parse(args)
 
@@ -74,16 +88,7 @@ class Emailer {
 			System.exit(0)
 		}
 
-		if( (options.fromAddr) ) {
-			println "\n${options.fromAddr}\n"
-			System.exit(0)
-		}
-
-		if( (options.template) ) {
-			println "\n${options.template}\n"
-			System.exit(0)
-		}
-
+		
 		return options
 	}
 
@@ -96,7 +101,6 @@ class Emailer {
 		//The default file is not required, so if it doesn't exist don't throw an exception
 		if (defaultConfigFile.exists() && defaultConfigFile.canRead()) {
 			config = new ConfigSlurper().parse(defaultConfigFile.toURL())
-			return config
 		} else {
 			System.exit(1)
 		}
@@ -107,24 +111,59 @@ class Emailer {
 			config = config.merge(new ConfigSlurper().parse(newConfigFile.toURL()))
 		}
 
-		return config
+		if( options?.fromAddr ) {
+			config.put('fromAddr', options.fromAddr)
+		}
+
+		if( options?.template ) {
+			config.put('template', options.template)
+		}
+
+		if( options?.recipient ) {
+			config.put('recipient', options.recipient)
+		}
+
+		if( options?.recipientHdr ) {
+			config.put('recipientHdr', options.recipientHdr)
+		}
+
+		if( options?.subject ) {
+			config.put('subject', options.subject)
+		}
+
+		if( options?.sender ) {
+			config.put('sender', options.sender)
+		}
+
+		if( options?.inputFile ) {
+			config.put('inputFile', options.inputFile)
+		}
+
+		config
 	}
 
-	private static runTemplate (config,groups) {
-		def text = new File(config.templatePath).getText()
+	private static runTemplate (config,templateData) {
+		//read the template
+		def text = new File(config.template).getText()
+
+		//construct the template
 		def engine = new groovy.text.GStringTemplateEngine()
-		def template =  engine.createTemplate(text).make(groups)
+		def template =  engine.createTemplate(text).make(templateData)
+
+		//return stringified template
 		template = template.toString()
-		template
 	}
 
-	private static retrieveCSVContents(options) {
-   		def fstring = new File(options.inputFile).getText()
+	private static retrieveCSVContents(config) {
+		//read our data file
+   		def fstring = new File(config.inputFile).getText()
 		def data = parseCsv(fstring)
+		
+		//parse data one line per element into result
 		def result = []
 		for(line in data){
 			result+=([line])
-			}
+		}
 		result
 
 	}
@@ -133,7 +172,9 @@ class Emailer {
 		println("\nEmailer ERROR: ${errorString}\n")
 		System.exit(1)
 	}
-	private static sendEmail(props, templateText) {
+
+	private static sendEmail(config,templateText) {
+		def props = config.toProperties()
 
 		def session = Session.getDefaultInstance(props, null)
 
